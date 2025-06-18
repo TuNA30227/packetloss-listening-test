98b887a0cc7536f7a60bbd7db1fdc01bf0798a3b
TuNA30227 <168460909+TuNA30227@users.noreply.github.com>
2025-06-18 22:28:24 +0800

    fix: connect to Render MySQL

--- config/db.php
+++ config/db.php
@@ -2,9 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=your_db_name',
-    'username' => 'your_db_user',
-    'password' => 'your_db_password',
+    'dsn' => 'mysql:host=<RENDER_DB_HOST>;port=3306;dbname=<RENDER_DB_NAME>',
+    'username' => '<RENDER_DB_USER>',
+    'password' => '<RENDER_DB_PASSWORD>',
     'charset' => 'utf8mb4',
 ];
+
 
